f1c43a9c6f142bac6b52d5dd1057df5cb8410757
heartbeating <786289616@qq.com>
2023-02-16 23:22:18 +0800

    first commit

---
+++ src/views/layout/Home.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="home">
+    <div class="menu-list">
+      <a-menu
+        :default-selected-keys="['1']"
+        :default-open-keys="['sub1']"
+        mode="inline"
+        theme="dark"
+        :inline-collapsed="collapsed"
+      >
+        <a-menu-item key="1">
+          <a-icon type="pie-chart" />
+          <span>Option 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="desktop" />
+          <span>Option 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <a-icon type="inbox" />
+          <span>Option 3</span>
+        </a-menu-item>
+        <a-sub-menu key="sub1">
+          <span slot="title"
+            ><a-icon type="mail" /><span>Navigation One</span></span
+          >
+          <a-menu-item key="5"> Option 5 </a-menu-item>
+          <a-menu-item key="6"> Option 6 </a-menu-item>
+          <a-menu-item key="7"> Option 7 </a-menu-item>
+          <a-menu-item key="8"> Option 8 </a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <span slot="title"
+            ><a-icon type="appstore" /><span>Navigation Two</span></span
+          >
+          <a-menu-item key="9"> Option 9 </a-menu-item>
+          <a-menu-item key="10"> Option 10 </a-menu-item>
+          <a-sub-menu key="sub3" title="Submenu">
+            <a-menu-item key="11"> Option 11 </a-menu-item>
+            <a-menu-item key="12"> Option 12 </a-menu-item>
+          </a-sub-menu>
+        </a-sub-menu>
+      </a-menu>
+    </div>
+    <div
+      :class="{
+        'main-app': true,
+        'menu-unfold': collapsed
+      }"
+    >
+      <div class="main-header">
+        <a-button
+          type="primary"
+          style="margin-bottom: 16px"
+          @click="toggleCollapsed"
+        >
+          <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'" />
+        </a-button>
+        <div class="breadcrumb">
+          <a-breadcrumb>
+            <a-breadcrumb-item>首页</a-breadcrumb-item>
+            <a-breadcrumb-item><a href="">统计</a></a-breadcrumb-item>
+          </a-breadcrumb>
+        </div>
+        <div class="user-info">
+          <a-dropdown>
+            <a class="ant-dropdown-link" @click="(e) => e.preventDefault()">
+              欢迎heartbeating<a-icon type="down" />
+            </a>
+            <a-menu slot="overlay" style="margin-top: 12px">
+              <a-menu-item>
+                <a href="javascript:;" @click="logoutHandle">退出登录</a>
+              </a-menu-item>
+            </a-menu>
+          </a-dropdown>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      collapsed: false
+    };
+  },
+  methods: {
+    toggleCollapsed() {
+      this.collapsed = !this.collapsed;
+    },
+    logoutHandle() {
+      console.log('22');
+    }
+  }
+};
+</script>
+
+<style lang="less">
+@import '~@/css/home.less';
+</style>
